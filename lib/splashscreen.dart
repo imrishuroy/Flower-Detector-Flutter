74883b7f1c02acf2cb14dce97e8b76a12e3204e2
Rishu Roy <rishuroy@Rishus-MacBook-Air.local>
2020-11-21 20:16:20 +0530

    UI of App Completed

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -8,6 +8,28 @@ class MySplashScreen extends StatelessWidget {
     return SplashScreen(
       seconds: 2,
       navigateAfterSeconds: HomeScreen(),
+      title: Text(
+        'Flower Detector',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 30.0,
+        ),
+      ),
+      image: Image.asset('assets/flower.png'),
+      photoSize: 50.0,
+      gradientBackground: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        stops: [0.004, 1],
+        colors: [
+          Color(0xffa8e063),
+          Color(0xff56ab2f),
+        ],
+      ),
+      loaderColor: Colors.white,
     );
   }
 }
+
+// 952347
